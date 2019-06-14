9584f08ca1074240ccfc8c585e7d24ecc6cd98bf
parashuram-patil <parasharam_patil@ymail.com>
2019-06-14 19:32:06 +0530

    On App init checking the login status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'AngularApp';
   loggedIn = false;
+  
+  ngOnInit() {
+    if(sessionStorage.getItem("loggedIn") == 'yes'){
+      this.loggedIn = true;
+    }
+  }
 }
